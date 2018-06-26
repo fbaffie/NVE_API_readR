4b7bd9fcd1eb05860a7e3ee79e7d533089a4775f
Florian Kobierska Baffie <fbaffie@gmail.com>
2018-06-26 14:05:19 +0200

    adding station names

--- R/query_api.R
+++ R/query_api.R
@@ -22,6 +22,33 @@
 #   "&nocache=0.8115327963068484&callback=dojo.io.script.jsonp_dojoIoScript8._jsonpCallback", sep = "")
 
 
+## Subfunction to parse and save the xml, which should maybe be outside of read_nve_api?
+parse_xml_and_save <- function(xgeo_url, nb_station, name_station) {
+  xmldata <- XML::xmlParse(xgeo_url)
+  tmp <- XML::xmlToDataFrame(XML::getNodeSet(xmldata,"//Point"),colClasses = c("character","double"))
+  
+  print(tmp)
+  
+  if (is.data.frame(tmp) && nrow(tmp) > 0) {
+    
+    time_vect <- tmp[ , 1]
+    
+    regine <- rep(nb_station, length(time))
+    name <- rep(name_station, length(time))
+    
+    ODM <- data.frame(regine.main = regine,
+                      station.name = name,
+                      nbname = paste(regine, "-", name, sep = ""),
+                      time = time_vect,
+                      Runoff_ODM.sim = tmp$Value)
+    
+    ODM[ODM == -9999.000] <- NA
+    ODM[ODM == -10000.000] <- NA
+    ODM <- dplyr::tbl_df(ODM)
+    invisible(ODM)
+  }
+}
+
 
 #' Draft function to call the NVE API
 #' Look also at http://nve-wiki.nve.no/index.php/Web_Chart_Service#da_.28DATA_ARCHIVE.29
@@ -37,7 +64,7 @@
 #' @importFrom XML xmlToDataFrame
 #' @importFrom XML getNodeSet
 #' @export
-read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_station = NULL, name_station = NULL, nb_variable = 6046) {
+read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_station = NULL, name_station = NULL) {  # , nb_variable = 6046
   # library(RCurl) for getURL
   
   # Load data from xgeo interface
@@ -49,32 +76,7 @@ read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_sta
   #   start_time <- "20160711T0600"
   #   stop_time <- "20160911T0600"
   
-  ## Subfunction to parse and save the xml, which should maybe be outside of read_nve_api?
-  parse_xml_and_save <- function(xgeo_url) {
-    xmldata <- XML::xmlParse(xgeo_url)
-    tmp <- XML::xmlToDataFrame(XML::getNodeSet(xmldata,"//Point"),colClasses = c("character","double"))
-    
-    print(tmp)
-    
-    if (is.data.frame(tmp) && nrow(tmp) > 0) {
-      
-      time_vect <- tmp[ , 1]
-      
-      regine <- rep(nb_station, length(time))
-      name <- rep(name_station, length(time))
-      
-      ODM <- data.frame(regine.main = regine,
-                        station.name = name,
-                        nbname = paste(regine, "-", name, sep = ""),
-                        time = time_vect,
-                        Runoff_ODM.sim = tmp$Value)
-      
-      ODM[ODM == -9999.000] <- NA
-      ODM[ODM == -10000.000] <- NA
-      ODM <- dplyr::tbl_df(ODM)
-      invisible(ODM)
-    }
-  }
+ 
   
   
   if (is.null(dat) == TRUE) {
@@ -82,19 +84,29 @@ read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_sta
     # nb_variable <- 1001  # this is measured discharge
     # nb_variable <- 6046  # For ODM results. 6011 gives same result.
     
-    # address for ODM tset
-    xgeo_url <- paste("http://h-web01.nve.no/chartserver/ShowData.aspx?req=getchart&vfmt=xml&ver=1.0&time=",
+    # address for ODM corrected modelled discharge
+    xgeo_url_corr_modQ <- paste("http://h-web01.nve.no/chartserver/ShowData.aspx?req=getchart&vfmt=xml&ver=1.0&time=",
                       start_time,";",stop_time,
                       "&chd=ds=tsr,da=",da,",id=vepshydra[1.",nb_station,
-                      ".0.",nb_variable,".0],rt=1.0:0,cht=line,mth=mean", sep = "")
+                      ".0.", 6046, ".0],rt=1.0:0,cht=line,mth=mean", sep = "")
     
-    print(xgeo_url)
+    # address for ODM air temperature   
+    xgeo_url_Temp <- paste("http://h-web01.nve.no/chartserver/ShowData.aspx?req=getchart&vfmt=xml&ver=1.0&time=",
+                                start_time,";",stop_time,
+                                "&chd=ds=tsr,da=",da,",id=vepshydra[1.",nb_station,
+                                ".0.", 6000, ".0],rt=1.0:0,cht=line,mth=mean", sep = "")
+    # address for ODM modelled SWE
+    xgeo_url_SWE <- paste("http://h-web01.nve.no/chartserver/ShowData.aspx?req=getchart&vfmt=xml&ver=1.0&time=",
+                                start_time,";",stop_time,
+                                "&chd=ds=tsr,da=",da,",id=vepshydra[1.",nb_station,
+                                ".0.", 6005, ".0],rt=1.0:0,cht=line,mth=mean", sep = "")
+    
+    # print(xgeo_url)
     # xgeo_url <- getURL(xgeo_url)
     
     
-    failsafe <- failwith(NULL,
-                         parse_xml_and_save)
-    failsafe(xgeo_url)
+    failsafe <- failwith(NULL, parse_xml_and_save)
+    failsafe(xgeo_url_corr_modQ, nb_station, name_station)
     
     # parse_xml_and_save(xgeo_url)
     
@@ -111,7 +123,7 @@ read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_sta
                       "time=",start_time,";",stop_time,
                       "&chd=ds=htsr,da=",da,",id=",nb_station,".0.",nb_variable,".",version,",rt=1&vfmt=xml",sep="")
     
-    xmldata <- XML::xmlParse(as.charachter(xgeo_url))
+    xmldata <- XML::xmlParse(as.charachter(xgeo_url_corr_modQ))
     tmp <- XML::xmlToDataFrame(XML::getNodeSet(xmldata,"//Point"),colClasses = c("character","double"))
     
     if (length(dat$time_vec) == nrow(tmp) ) {
@@ -125,7 +137,13 @@ read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_sta
 }
 
 #' Function which loads ODM model results from the NVE REST API
-#'
+#' Parameter codes for ODM:
+#' 6000 (mean) air temperature
+#' 6004 (mean) Modelled discharge
+#' 6005 (mean) Modelled SWE
+#' 6014 (mean) Modelled soil water deficit
+#' 6046 (mean) Corrected modelled discharge
+#' 6001 (sum) Modelled precipitation
 #' @param start_day
 #' @param stop_day
 #'
@@ -137,15 +155,14 @@ read_nve_api <- function(dat = NULL, start_time = NULL, stop_time = NULL, nb_sta
 #' ODM <- tidyr::gather(ODM, key = Tmp, value = Values, -time, -regine.main, -station.name, -nbname) %>%
 #' tidyr::separate(Tmp, into = c("Type", "Variable"), sep = "_")
 
-load_ODM <- function(start_day = Sys.Date() + 1, stop_day = Sys.Date()+10) {
+read_ODM_api <- function(start_day = Sys.Date() + 1, stop_day = Sys.Date()+10) {
   # Used to be start_day = Sys.Date()-20 but the historical data was the same as the observation rather than being old model results.
-  # Ask Bard whether it is a mistake from my understanding of the NVE REST API or whether he just decided to organize his data this way
-  
+
   # Get the regine numbers related to the station names in the HBV output file
   # Read it from the package for use anywhere
-  station_ref <- read.table(system.file("demodata/usikkerhet_grd", "HbvFelt147.txt", package = "NVEDATA"))
+  station_ref <- read.table("data/HbvFelt147.txt")
   regine_ref_nb <- paste(station_ref$V1, ".", station_ref$V2, sep = "")
-  
+
   station_ref_name <- station_ref$V5
   
   tmp_start <- strsplit(as.character(start_day), split = "-")
@@ -163,8 +180,9 @@ load_ODM <- function(start_day = Sys.Date() + 1, stop_day = Sys.Date()+10) {
                     Runoff_ODM.sim = c())
   
   for (i in seq(along = regine_ref_nb)) {
-    single_st_ODM <- read_nve_api(data = NULL, start_time = start_time, stop_time = stop_time, nb_station = regine_ref_nb[i],
-                                  name_station = station_ref_name[i], nb_variable = 6046)
+    single_st_ODM <- read_nve_api(dat = NULL, start_time = start_time, stop_time = stop_time, nb_station = regine_ref_nb[i],
+                                  name_station = station_ref_name[i]) # , nb_variable = 6046
+    
     ODM <- dplyr::bind_rows(ODM, single_st_ODM)
   }
   
